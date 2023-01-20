8be9080c27e8e3173cd5ac8e9cb895887d1bfc3f
100Niraj <nirajkumarpatel264@gmail.com>
2023-01-21 00:44:19 +0530

    added Lesson_1

--- src/index.js
+++ src/index.js
@@ -11,10 +11,32 @@
 //   </React.StrictMode>
 // );
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+//var React = require('react');
+//var ReactDOM = require('react-dom');
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello Niraj !</h1>, document.getElementById("root"));
+//jsx ;- javascrip xml
+
+//ReactDOM.render(<h1>Hello Niraj !</h1>, document.getElementById("root"));
+
+//concept of js to run 
+/*var h1 = document.createElement("h1");
+h1.innerHTML = "Hello Niraj";
+document.getElementById("root").appendChild(h1); */
+
+const arr = ['niraj', 'deepak', 'khushboo'];
+
+//in react v16 it's possible for render() without use div<> in html
+//to return an array of elemnts;
+
+ReactDOM.render(
+  [
+    <h1>Hello Niraj !</h1>,
+    <p>How are you?</p>
+  ],
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
