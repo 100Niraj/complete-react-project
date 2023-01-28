f3b8fc545221477c0d9523c7bbc28fdd53cad6a4
100Niraj <nirajkumarpatel264@gmail.com>
2023-01-28 14:26:44 +0530

    Added Lesson_12

--- src/Profile.js
+++ src/Profile.js
@@ -15,26 +15,49 @@ import React from "react";
 
 
 class Profile extends React.Component{
-    constructor()
+    constructor() 
     {
         super();
+        // this.name = React.createRef();
+        // this.password = React.createRef();
         this.state ={
-            show : true
+            name: "",
+            password: "",
         }
+
     }
-    render(){
+    // submitHandle(event)
+    // {
+    //     event.preventDefault();
+    //     console.warn(this.name.current.value , this.password.current.valure)
+    // }
+
+    submit(){
+        console.log(this.state)
+    }
+    render()
+    {
         return (
             <div>
-            {
-                this.state.show ?
-                <h1> Hide and Show</h1>
-                :null
+                <h1>Login  Form</h1>
+                {/* <form onSubmit={(event) => {this.submitHandle(event)}}>
+                    <input type="Name" placeholder="enter name" ref={this.name}></input> <br></br>
+                    <br></br>
+                    <input type="password" placeholder="enter Password" ref={this.password}></input><br></br>
+                    <br></br> */}
+                    <input type="text" placeholder = "enter name"
+                    onChange={(event) => {this.setState({name:event.target.value})} }/><br/> <br />
+                    <input type="text" placeholder = "password"
+                    onChange={(event) => {this.setState({password:event.target.value})} }/><br/> <br />
 
-            }
-            <button onClick={()=> {this.setState({show:! this.state.show})}}>click me </button>
+
+
+                    <button onClick= {() => {this.submit()}}> Submit</button>
+                
             </div>
-        );
+        )
     }
+    
 }
 
 export default Profile;
